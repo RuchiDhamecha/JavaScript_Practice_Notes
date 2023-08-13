a98953790c557a238463d1a2b1c8e22ed11b915c
RuchiDhamecha <87808663+RuchiDhamecha@users.noreply.github.com>
2023-08-14 00:17:49 +0530

    added example of undefined

--- null_undefined_empty.js
+++ null_undefined_empty.js
@@ -2,3 +2,20 @@
 
 //null =
 //example
+
+
+
+// undefined =======================================================
+//example1
+let a;   // value is not asigned so js cant recognize datatype
+console.log(typeof a) //undefined
+
+//example2
+let obj={
+    color :"blue"
+}
+console.log(obj.colour)  //due to spell mistake it becomes different key value
+console.log(obj.font);  //undefined as font not declared
+
+// empty =============================================================
+
